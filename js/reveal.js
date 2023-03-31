2748a8a0cbdecfb6f96d9717218f3f9ed7609f67
naoproduction <122692612+naoproduction@users.noreply.github.com>
2023-03-31 22:22:42 +0200

    Add files via upload

--- js/reveal.js
+++ js/reveal.js
@@ -1,10 +1,15 @@
+function showHome() {
+	document.getElementById("home-text").style.visibility = "visible";
+  }
+  setTimeout("showHome()", 250); // aprés 25ms
+
 const sr = ScrollReveal ({
 	distance: '45px',
 	duration: 2200,
 	reset: true
 })
 
-sr.reveal('.home-text',{delay:350, origin:'bottom'})
-sr.reveal('.home-img',{delay:350, origin:'right'})
+sr.reveal('.home-text',{delay:150, origin:'bottom'})
+sr.reveal('.home-img',{delay:150, origin:'right'})
 
-sr.reveal('.sub-service,.about,.portfolio,.service,.cta,.contact,.ftco-section',{delay:130, origin:'bottom'})
+sr.reveal('.sub-service,.about,.portfolio,.service,.cta,.contact,.ftco-section',{delay:80, origin:'bottom'})
--- js/scroll.js
+++ js/scroll.js
@@ -17,4 +17,37 @@ window.onscroll = () => {
 	navlist.classList.remove('active');
 };
 
+// 
+
+let pages = document.querySelectorAll("section");
+let nav = document.querySelectorAll("nav a");
+let navContact = document.querySelectorAll("nav span");
+
+function getTopOfElement(element) {
+  return (
+    element.getBoundingClientRect().top -
+    document.body.getBoundingClientRect().top
+  );
+}
+
+function setPageActive(scrollPosition) {
+  for (let page of pages) {
+    let bottom = getTopOfElement(page) + page.clientHeight;
+
+    for (let anchor of nav) {
+      if (scrollPosition >= getTopOfElement(page) && scrollPosition <= bottom) {
+        anchor.hash.split("#")[1] === page.id
+          ? anchor.classList.add("active")
+          : anchor.classList.remove("active");
+      }
+    }
+  }
+}
+
+setPageActive(window.scrollY);
+
+window.addEventListener("scroll", event => {
+  setPageActive(window.scrollY);
+});
+
 
